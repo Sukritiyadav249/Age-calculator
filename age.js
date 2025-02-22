7f11cf06a9012072437c3104f0afac4b270d41ae
Sukriti Yadav <sukriti24@navgurukul.org>
2025-02-22 19:14:31 +0530

    Your commit message

---
+++ age.js
@@ -0,0 +1,36 @@
+function calculateAge() {
+  let day = document.getElementById("day").value;
+  let month = document.getElementById("month").value;
+  let year = document.getElementById("year").value;
+
+  if (day === "" || month === "" || year === "") {
+    document.getElementById("result").innerHTML = "please enter a valid date!";
+    return;
+  }
+  let birthDate = new Date(year, month - 1, day);
+  let today = new Date();
+
+  let ageYears = today.getFullYear() - birthDate.getFullYear();
+  let ageMonths = today.getMonth() - birthDate.getMonth();
+  let ageDays = today.getDays() - birthDate.getDate();
+
+  if (ageDays < 0) {
+    ageMonths--;
+    ageDays += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+  }
+
+  if (ageMonths < 0) {
+    ageYears--;
+    ageMonths += 12;
+  }
+  document.getElementById(
+    "result"
+  ).innerHTML = `You are ${ageYears}years, ${ageMonths}months, and ${ageDays} days old.`;
+}
+
+function resetFields() {
+  document.getElementById("day").value = "";
+  document.getElementById("month").value = "";
+  document.getElementById("year").value = "";
+  document.getElementById("result").innerHTML = "";
+}
